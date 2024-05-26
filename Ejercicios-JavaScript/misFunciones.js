7e2d2345c227eca297d4a4da2f5c92eb1c33e8e3
FrancoNicEsterio <2302320@ucc.edu.ar>
2024-05-26 19:11:37 -0300

    2do ej JavaScript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,28 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+function cambiarUnidades(id, valor){
+if(isNaN(valor)){
+      alert("Se ingresó un valor inválido");
+       document.lasUnidades.unid_metro.value = "";
+       document.lasUnidades.unid_pulgada.value = "";
+       document.lasUnidades.unid_pie.value = "";
+       document.lasUnidades.unid_yarda.value = "";
+     }else if(id=="metro"){
+       document.lasUnidades.unid_pulgada.value = 39.3701*valor;
+       document.lasUnidades.unid_pie.value = 3.28084*valor;
+       document.lasUnidades.unid_yarda.value = 1.09361*valor;
+      }else if (id=="pulgada"){
+       document.lasUnidades.unid_metro.value = 0.0254*valor;
+       document.lasUnidades.unid_pie.value = 0.0833333*valor;
+       document.lasUnidades.unid_yarda.value = 0.0277778*valor;
+     }else if (id=="yarda"){
+       document.lasUnidades.unid_metro.value = 0.9144*valor;
+       document.lasUnidades.unid_pie.value = 3*valor;
+       document.lasUnidades.unid_pulgada.value = 36*valor;
+     }else if (id=="pie"){
+       document.lasUnidades.unid_metro.value = 0.3048*valor;
+       document.lasUnidades.unid_yarda.value = 0.333333*valor;
+       document.lasUnidades.unid_pulgada.value = 12*valor;
+     }
+}
