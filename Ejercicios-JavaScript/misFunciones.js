8e05a2c462f135e20a25042e0e7f41e62d395fe8
FrancoNicEsterio <2302320@ucc.edu.ar>
2024-05-27 15:40:59 -0300

    Grados a radianes. JavaScript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -30,3 +30,16 @@ if(isNaN(valor)){
        document.lasUnidades.unid_pulgada.value = 12*valor;
      }
 }
+
+function convertirGR(id){
+    var grad, rad;
+    if(id=="grados"){
+        grad = document.getElementById("grados").value;
+        rad = (grad*Math.PI)/180;
+    }else if (id=="radianes"){
+       rad = document.getElementById("radianes").value;
+       grad = (rad*180)/Math.PI
+    }
+    document.getElementById("grados").value = grad;
+    document.getElementById("radianes").value = rad;
+}
