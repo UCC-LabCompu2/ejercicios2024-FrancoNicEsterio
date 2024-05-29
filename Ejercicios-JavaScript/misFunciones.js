f643d01e6f90f4efb7b6b8a97a7132f0f4f0ab8e
FrancoNicEsterio <2302320@ucc.edu.ar>
2024-05-29 13:21:13 -0300

    Ej java operaciones matematicas

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -52,3 +52,35 @@ function mostrar_ocultar(valorMo){
         document.getElementById("divMo").style.display = 'none';
     }
 }
+
+function calcularSuma(){
+    var num1, num2;
+
+    num1=Number (document.getElementsByName("sum_num1")[0].value); // puede ser de las 2 maneras (Esta)
+    num2=document.getElementsByName("sum_num2")[0].value;
+    document.getElementsByName("sum_total")[0].value = num1 + Number(num2); // o esta.
+}
+
+function calcularResta(){
+    var num1, num2;
+
+    num1=Number (document.getElementsByName("res_num1")[0].value);
+    num2=Number (document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].value = num1 - num2;
+}
+
+function calcularMultiplicacion(){
+    var num1, num2;
+
+    num1= document.getElementsByName("mul_num1")[0].value;
+    num2= document.getElementsByName("mul_num2")[0].value;
+    document.getElementsByName("mul_total")[0].value = Number(num1) * Number(num2);
+}
+
+function calcularDivision(){
+    var num1, num2;
+
+    num1= Number (document.getElementsByName("div_num1")[0].value);
+    num2= document.getElementsByName("div_num2")[0].value;
+    document.getElementsByName("div_total")[0].value = num1 / Number(num2);
+}
