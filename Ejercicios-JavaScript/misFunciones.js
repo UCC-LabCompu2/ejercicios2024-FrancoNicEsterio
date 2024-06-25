730087da5789f5586e623f25aa21c5e5dff5d58a
FrancoNicEsterio <2302320@ucc.edu.ar>
2024-06-25 16:15:32 -0300

    Animacion de canvas SetInterval

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -241,3 +241,26 @@ function dibujarImagen(posX,posY) {
 
     }
 }
+
+x=0;
+dx=2;
+function animarAuto(){
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    console.log(posX, posY);
+
+    canvas.width = img.width;
+
+    var img = new Image();
+    img.src = "images/auto.png";
+
+
+    img.onload = function () {
+        ctx.drawImage(img,x,100);
+
+    }
+    if(x>canvas.width){
+        x=0;
+    }
+    x+=dx;
+}
